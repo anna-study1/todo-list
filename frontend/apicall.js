99ce57db01740d31424bbb9c7040a2cf657edf58
Anna <anna1202@gmail.com>
2023-07-26 22:28:13 -0500

    added apicall.js file

---
+++ frontend/apicall.js
@@ -0,0 +1,47 @@
+function sendNoteToServer(note) {
+    const requestOptions = {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(note)
+    };
+
+    return fetch(domain+ '/rest/notes', requestOptions)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data.id);
+            return data; // Pass the data to the next promise in the chain
+        });
+}
+
+function updateNoteOnServer(note) {
+    const requestOptions = {
+        method: 'PUT',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(note)
+    };
+    return fetch(domain+ '/rest/notes/' + note.id, requestOptions);
+}
+
+function getNoteFromServer() {
+    const requestOptions = {
+        method: 'GET',
+        headers: {'Content-Type': 'application/json'},
+    };
+
+    return fetch(domain+ '/rest/notes', requestOptions)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            notes = data;
+            return data; // Pass the data to the next promise in the chain
+        });
+}
+
+function deleteFromServer(id) {
+    const requestOptions = {
+        method: 'DELETE',
+        headers: {'Content-Type': 'application/json'},
+    };
+
+    return fetch(domain+ '/rest/notes/' + id, requestOptions);
+}
--- frontend/index.js
+++ frontend/index.js
@@ -47,45 +47,7 @@ function saveNote() {
         });
 }
 
-function sendNoteToServer(note) {
-    const requestOptions = {
-        method: 'POST',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify(note)
-    };
 
-    return fetch(domain+ '/rest/notes', requestOptions)
-        .then(response => response.json())
-        .then(data => {
-            console.log(data.id);
-            return data; // Pass the data to the next promise in the chain
-        });
-}
-
-function updateNoteOnServer(note) {
-    const requestOptions = {
-        method: 'PUT',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify(note)
-    };
-    return fetch(domain+ '/rest/notes/' + note.id, requestOptions);
-
-}
-
-function getNoteFromServer() {
-    const requestOptions = {
-        method: 'GET',
-        headers: {'Content-Type': 'application/json'},
-    };
-
-    return fetch(domain+ '/rest/notes', requestOptions)
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
-            notes = data;
-            return data; // Pass the data to the next promise in the chain
-        });
-}
 
 function clearInputNote() {
     document.getElementById('new-note').value = '';
@@ -177,15 +139,7 @@ function deleteNote() {
         });
 }
 
-function deleteFromServer(id) {
-    const requestOptions = {
-        method: 'DELETE',
-        headers: {'Content-Type': 'application/json'},
-    };
 
-    return fetch(domain+ '/rest/notes/' + id, requestOptions);
-
-}
 
 function addEventToEditButtons() {
     let editButtons = document.querySelectorAll("[id^='btnEdit-']");
